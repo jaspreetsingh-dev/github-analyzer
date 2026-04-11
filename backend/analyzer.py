59e270fa83c3fc6d663a2bcf427d669c25c44fb4
jaspreetsingh-dev <jaspreetsinghh.dev@gmail.com>
2026-04-11 22:24:26 +0530

    add calculate_codex_score function with weighted formula

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -17,3 +17,28 @@ def calculate_basic_stats(repos):
         "total_forks": total_forks,
         "languages": languages
     }
+
+def calculate_codex_score(profile, repos):
+    total_repos = len(repos)
+    total_stars = sum(repo["stargazers_count"] for repo in repos)
+    total_forks = sum(repo["forks_count"] for repo in repos if repo["language"])
+    followers = profile["followers"]
+    languages = set(repo["language"] for repo in repos if repo["language"])
+    described = sum(1 for repo in repos if repo["description"])
+
+    star_score = min(total_stars/ 1000 * 100, 100)
+    community_score = min((followers + total_forks) / 500 * 100, 100)
+    consistency_score = min(total_repos / 30 *100, 100)
+    doc_score = (described / total_repos * 100) if total_repos > 0 else 0
+    diversity_score = min(len(languages) / 10 * 100, 100)
+
+    codex_score = (
+        star_score * 0.25 +
+        community_score * 0.20 +
+        consistency_score * 0.20 +
+        doc_score * 0.20 +
+        diversity_score * 0.15 
+    )
+
+    return round(codex_score, 2)
+
